d931607177925d55ecd2867ed56237534a0fb751
JeanelleSebastian <jeanellesebastian@gmail.com>
2017-09-27 01:28:53 -0700

    Replaced all cats with burgers, running locally

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Shake Shack", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("In n Out", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Five Guys", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Eureka", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Burger Lounge", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Habit Burger", False);
+
